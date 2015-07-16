74a3e8187d3eaa86fe0b95d6f4a97fb0004de58b
storykim <shanytt@gmail.com>
2015-07-16 15:19:48 +0900

    add oauth scope description

--- sparcssso/settings.py
+++ sparcssso/settings.py
@@ -35,6 +35,11 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope :)', 'write': 'Write scope :)', 'groups': 'Access to your groups'}
+}
+
 
 # Application definition
 

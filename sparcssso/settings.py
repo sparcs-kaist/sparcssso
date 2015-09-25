97a4d66a930f2b14f6782a784c86664dcfea37ae
Sam Jo <a1sams1a@gmail.com>
2015-09-25 21:50:25 +0900

    add media in real settings

--- sparcssso/settings.py
+++ sparcssso/settings.py
@@ -133,7 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/media/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

1b7142853e9fbd4aca8f08f48cf8bada71569f01
Sam Jo <a1sams1a@gmail.com>
2016-06-05 21:39:14 +0900

    kill v0 api + redirect. close issue #68

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls import url
-from apps.api.views import v0, v1
+from apps.api.views import v1
 
 urlpatterns = [
     url(r'^versions/$', v1.versions),
-
-    # VERSION 0 #
-    url(r'^token/require/$', v0.token_require),
-    url(r'^token/info/$', v0.token_info),
-    url(r'^point/$', v0.point),
-    url(r'^email/$', v0.email),
 
     # VERSION 1 #
     url(r'^v1/logout/$', v1.logout),

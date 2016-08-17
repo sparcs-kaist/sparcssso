18b8e389dd273bbff1ceef27fb310fd7eaaf5543
todo Kim <todo@sparcs.org>
2016-08-18 01:48:21 +0900

    initial commit of todo

--- apps/core/views/general.py
+++ apps/core/views/general.py
@@ -77,7 +77,7 @@ def stats(request):
         else:
             stat.append(s)
 
-    return render(request, 'stats.html', {'level': level, 'time': time, 'stat': stat})
+    return render(request, 'new_stats.html', {'level': level, 'time': time, 'stat': stat})
 
 
 # /help/

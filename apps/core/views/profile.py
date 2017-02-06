4e80e8b252e64ade0a25bb43b8293d6f0499ab49
Sam Jo <samjo.nyang@gmail.com>
2017-02-06 22:57:34 +0900

    front refactoring in progress (done /account/)

--- apps/core/views/profile.py
+++ apps/core/views/profile.py
@@ -28,6 +28,9 @@ def main(request):
             if user.email != email:
                 user.email = email
                 user.profile.email_authed = False
+                tokens = EmailAuthToken.objects.filter(user=user).all()
+                for token in tokens:
+                    token.delete()
 
             user.first_name = user_f.cleaned_data['first_name']
             user.last_name = user_f.cleaned_data['last_name']

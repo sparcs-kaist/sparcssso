1062cd5f3c48d49e39da8fd13b9cf836d30f2a72
Sam Jo <samjo.nyang@gmail.com>
2017-06-04 17:58:38 +0900

    change buffer dir to /archive/buffer/; rename etc -> Custom

--- sparcssso/settings.py
+++ sparcssso/settings.py
@@ -169,7 +169,7 @@ ADMINS = (('SSO SYSOP', 'sso.sysop@sparcs.org'),)
 
 LOG_DIR = os.path.join(BASE_DIR, 'archive/logs/')
 
-LOG_BUFFER_DIR = os.path.join(LOG_DIR, 'buffer/')
+LOG_BUFFER_DIR = os.path.join(BASE_DIR, 'archive/buffer/')
 
 LOGGING = {
     'version': 1,

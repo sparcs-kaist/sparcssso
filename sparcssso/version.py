4d0faf0adc99e0040845cac81aedeca9a64ff217
Sam Jo <samjo.nyang@gmail.com>
2018-09-07 22:22:12 +0900

    Fix version string

--- sparcssso/version.py
+++ sparcssso/version.py
@@ -3,31 +3,30 @@ import subprocess
 from datetime import datetime
 
 
-def get_version_info():
-    log_re = re.compile('^"([0-9a-f]{40}) ([^\(]+) \((.+)\)"$')
+def get_version_info(debug, allowed_hosts):
+    log_re = re.compile('^([\da-f]{40}) ([^\(]+) .+$')
     try:
-        out, err = subprocess.Popen(
-            ['git', 'log', '-1', '--pretty="%H %ci%d"'],
-            stdout=subprocess.PIPE,
-        ).communicate()
-        if err:
-            raise RuntimeError()
-
-        match_obj = log_re.match(out.decode().strip())
+        proc = subprocess.run(
+            'git log -1 --pretty="%H %ci%d"',
+            stdout=subprocess.PIPE, shell=True)
+        match_obj = log_re.match(proc.stdout.decode().strip())
         git_hash = match_obj.group(1)[:8]
         time = datetime.strptime(
             match_obj.group(2),
             '%Y-%m-%d %H:%M:%S %z',
         )
-        branch = match_obj.group(3)
-    except:
+        proc = subprocess.run(
+            'git status --porcelain',
+            stdout=subprocess.PIPE, shell=True)
+        dirty = bool(proc.stdout.decode().strip())
+    except Exception:
         return 'unknown-00000000T000000-00000000'
 
-    if 'master' in branch:
+    if 'sso.sparcs.org' in allowed_hosts:
         prefix = 'release'
-    elif 'develop' in branch:
+    elif 'ssobeta.sparcs.org' in allowed_hosts:
         prefix = 'beta'
     else:
-        prefix = 'nightly'
+        prefix = 'dev'
     simple_time = time.strftime('%Y%m%dT%H%m%S')
-    return f'{prefix}-{simple_time}-{git_hash}'
+    return f'{prefix}-{simple_time}-{git_hash}{":dirty" if dirty else ""}'

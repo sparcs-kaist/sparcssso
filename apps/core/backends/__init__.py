b5c164818b0b1c38d261aa01b8cfb545c37ba8f5
Sam Jo <samjo.nyang@gmail.com>
2018-09-12 22:33:23 +0900

    Add Travis CI
    - Check both Python and JS lint
    - Change Python import style from edited to pycharm
    - Increase max line length from 80 to 120

--- apps/core/backends/__init__.py
+++ apps/core/backends/__init__.py
@@ -1,7 +1,7 @@
-from apps.core.backends.auth import *      # noqa: F401, F403
-from apps.core.backends.decorator import * # noqa: F401, F403
-from apps.core.backends.service import *   # noqa: F401, F403
-from apps.core.backends.signup import *    # noqa: F401, F403
-from apps.core.backends.token import *     # noqa: F401, F403
-from apps.core.backends.user import *      # noqa: F401, F403
-from apps.core.backends.util import *      # noqa: F401, F403
+from apps.core.backends.auth import *       # noqa: F401, F403
+from apps.core.backends.decorator import *  # noqa: F401, F403
+from apps.core.backends.service import *    # noqa: F401, F403
+from apps.core.backends.signup import *     # noqa: F401, F403
+from apps.core.backends.token import *      # noqa: F401, F403
+from apps.core.backends.user import *       # noqa: F401, F403
+from apps.core.backends.util import *       # noqa: F401, F403

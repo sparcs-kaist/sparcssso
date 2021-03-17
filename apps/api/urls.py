7b492aececc69b32998781c1eb494364a94bdfa1
Junghoon Noh <jungnoh.dev@gmail.com>
2021-03-17 20:59:16 +0900

    Use class based views for partial v2 apis

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -20,11 +20,11 @@ urlpatterns = [
     path('versions/', versions),
 
     # VERSION 2 #
-    path('v2/token/require/', v2.token_require),
-    path('v2/token/info/', v2.token_info),
+    path('v2/token/require/', v2.TokenRequireView.as_view()),
+    path('v2/token/info/', v2.TokenInfoView.as_view()),
     path('v2/logout/', v2.logout),
     path('v2/point/', v2.point),
-    path('v2/notice/', v2.notice),
-    path('v2/email/', v2.email),
+    path('v2/notice/', v2.NoticeView.as_view()),
+    path('v2/email/', v2.EmailView.as_view()),
     path('v2/stats/', v2.stats),
 ]

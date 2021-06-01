5f41048f75e2088fc3fb4e48fc77f470facee5bd
Junghoon Noh <jungnoh.dev@gmail.com>
2021-06-01 21:03:18 +0900

    Move env loading to settings __init__

--- manage.py
+++ manage.py
@@ -2,14 +2,6 @@
 import os
 import sys
 
-
-if os.environ.get('SSO_LOCAL', '0') == '1':
-    from dotenv import load_dotenv
-    load_dotenv(override=True)
-    if os.environ.get('SSO_ENV') != 'development':
-        print('SSO_ENV must be development if SSO_LOCAL is true.')
-        exit(1)
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sparcssso.settings")
 
--- sparcssso/settings/__init__.py
+++ sparcssso/settings/__init__.py
@@ -1,5 +1,11 @@
 import os
 
+if os.environ.get('SSO_LOCAL', '0') == '1':
+    from dotenv import load_dotenv
+    load_dotenv(override=True)
+    if os.environ.get('SSO_ENV') != 'development':
+        print('SSO_ENV must be development if SSO_LOCAL is true.')
+        exit(1)
 
 if os.environ.get('SSO_ENV') == "development":
     from .development import *  # noqa: F401, F403

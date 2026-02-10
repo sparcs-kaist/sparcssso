db2e6b4d0f80873748249a0b2bbf8dd2bc14631e
hyukychang <seunghyuk.dev@gmail.com>
2026-02-11 01:55:25 +0900

    formatting

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -3,24 +3,33 @@ from django.contrib.auth import admin as uadmin
 from django.contrib.auth.models import User
 
 from apps.core.models import (
-    AccessToken, Document, EmailAuthToken, EmailDomain, InquiryMail, Notice,
-    PointLog, ResetPWToken, Service, ServiceMap,
-    Statistic, UserLog, UserProfile,
+    AccessToken,
+    Document,
+    EmailAuthToken,
+    EmailDomain,
+    InquiryMail,
+    Notice,
+    PointLog,
+    ResetPWToken,
+    Service,
+    ServiceMap,
+    Statistic,
+    UserLog,
+    UserProfile,
 )
-
 
 admin.site.unregister(User)
 
 
 # Filters
 class UserFilter(admin.SimpleListFilter):
-    title = 'user'
-    parameter_name = 'user'
+    title = "user"
+    parameter_name = "user"
 
     def lookups(self, request, model_admin):
         users = [u.user for u in model_admin.model.objects.all()]
         users = list(set(users))
-        return [(u.id, u.last_name + ' ' + u.first_name) for u in users]
+        return [(u.id, u.last_name + " " + u.first_name) for u in users]
 
     def queryset(self, request, qs):
         if self.value():
@@ -32,12 +41,12 @@ class UserFilter(admin.SimpleListFilter):
 # Admin for General Objects
 @admin.register(Notice)
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'valid_from', 'valid_to', 'text')
+    list_display = ("title", "valid_from", "valid_to", "text")
 
 
 @admin.register(Statistic)
 class StatisticAdmin(admin.ModelAdmin):
-    list_display = ('time', 'data')
+    list_display = ("time", "data")
     readonly_fields = list(map(lambda x: x.name, Statistic._meta.fields))
 
     def has_add_permission(self, request, obj=None):
@@ -46,24 +55,31 @@ class StatisticAdmin(admin.ModelAdmin):
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('category', 'version', 'date_apply', 'date_version')
+    list_display = ("category", "version", "date_apply", "date_version")
 
 
 # Admin for Service Related Objects
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_shown', 'alias', 'scope',
-                    'admin_user', 'main_url', 'login_callback_url',
-                    'unregister_url', 'cooltime')
+    list_display = (
+        "name",
+        "is_shown",
+        "alias",
+        "scope",
+        "admin_user",
+        "main_url",
+        "login_callback_url",
+        "unregister_url",
+        "cooltime",
+    )
 
 
 @admin.register(ServiceMap)
 class ServiceMapAdmin(admin.ModelAdmin):
     actions = None
-    list_display = ('sid', 'user', 'service',
-                    'register_time', 'unregister_time')
-    readonly_fields = ('user', 'service', 'sid')
-    search_fields = ('user__email', 'service__name', 'service__alias')
+    list_display = ("sid", "user", "service", "register_time", "unregister_time")
+    readonly_fields = ("user", "service", "sid")
+    search_fields = ("user__email", "service__name", "service__alias")
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -74,8 +90,8 @@ class ServiceMapAdmin(admin.ModelAdmin):
 
 @admin.register(AccessToken)
 class AccessTokenAdmin(admin.ModelAdmin):
-    list_display = ('tokenid', 'user', 'service', 'expire_time')
-    list_filter = (UserFilter, )
+    list_display = ("tokenid", "user", "service", "expire_time")
+    list_filter = (UserFilter,)
     readonly_fields = list(map(lambda x: x.name, AccessToken._meta.fields))
 
     def has_add_permission(self, request, obj=None):
@@ -89,58 +105,88 @@ class UserAdmin(uadmin.UserAdmin):
         model = UserProfile
         can_delete = False
         fieldsets = (
-            ('General', {
-                'fields': ('gender', 'birthday',
-                           'email_authed', 'expire_time'),
-            }),
-            ('Points', {
-                'fields': ('point', 'point_test'),
-            }),
-            ('SNS', {
-                'fields': ('facebook_id', 'twitter_id',
-                           'kaist_id', 'kaist_info_time', 'kaist_info', 'kaist_v2_info_time', 'kaist_v2_info'),
-            }),
-            ('Dev / Debug', {
-                'fields': ('sparcs_id', 'test_only', 'test_enabled'),
-            }),
+            (
+                "General",
+                {
+                    "fields": ("gender", "birthday", "email_authed", "expire_time"),
+                },
+            ),
+            (
+                "Points",
+                {
+                    "fields": ("point", "point_test"),
+                },
+            ),
+            (
+                "SNS",
+                {
+                    "fields": (
+                        "facebook_id",
+                        "twitter_id",
+                        "kaist_id",
+                        "kaist_info_time",
+                        "kaist_info",
+                        "kaist_v2_info_time",
+                        "kaist_v2_info",
+                    ),
+                },
+            ),
+            (
+                "Dev / Debug",
+                {
+                    "fields": ("sparcs_id", "test_only", "test_enabled"),
+                },
+            ),
         )
 
     def get_profile(self, obj):
         return UserProfile.objects.get(user=obj)
 
     def get_name(self, obj):
-        return obj.last_name + ' ' + obj.first_name
-    get_name.admin_order_field = 'last_name'
-    get_name.short_description = 'Name'
+        return obj.last_name + " " + obj.first_name
+
+    get_name.admin_order_field = "last_name"
+    get_name.short_description = "Name"
 
     def get_gender(self, obj):
         return self.get_profile(obj).gender_display()
-    get_gender.short_description = 'Gender'
+
+    get_gender.short_description = "Gender"
 
     def get_point(self, obj):
         return self.get_profile(obj).point
-    get_point.short_description = 'Point'
+
+    get_point.short_description = "Point"
 
     def get_email_authed(self, obj):
         return self.get_profile(obj).email_authed
-    get_email_authed.short_description = 'Email Authed'
+
+    get_email_authed.short_description = "Email Authed"
     get_email_authed.boolean = True
 
     def get_test_enabled(self, obj):
         return self.get_profile(obj).test_enabled
-    get_test_enabled.short_description = 'Test Account'
+
+    get_test_enabled.short_description = "Test Account"
     get_test_enabled.boolean = True
 
-    list_display = ('email', 'username', 'get_name', 'get_gender', 'get_point',
-                    'get_email_authed', 'get_test_enabled')
-    list_filter = ('is_staff', )
-    inlines = (UserProfileInline, )
+    list_display = (
+        "email",
+        "username",
+        "get_name",
+        "get_gender",
+        "get_point",
+        "get_email_authed",
+        "get_test_enabled",
+    )
+    list_filter = ("is_staff",)
+    inlines = (UserProfileInline,)
     ordering = ()
 
 
 @admin.register(EmailAuthToken)
 class EmailAuthTokenAdmin(admin.ModelAdmin):
-    list_display = ('tokenid', 'expire_time', 'user')
+    list_display = ("tokenid", "expire_time", "user")
     readonly_fields = list(map(lambda x: x.name, EmailAuthToken._meta.fields))
 
     def has_add_permission(self, request, obj=None):
@@ -149,7 +195,7 @@ class EmailAuthTokenAdmin(admin.ModelAdmin):
 
 @admin.register(ResetPWToken)
 class ResetPWTokenAdmin(admin.ModelAdmin):
-    list_display = ('tokenid', 'expire_time', 'user')
+    list_display = ("tokenid", "expire_time", "user")
     readonly_fields = list(map(lambda x: x.name, ResetPWToken._meta.fields))
 
     def has_add_permission(self, request, obj=None):
@@ -159,7 +205,7 @@ class ResetPWTokenAdmin(admin.ModelAdmin):
 @admin.register(PointLog)
 class PointLogAdmin(admin.ModelAdmin):
     actions = None
-    list_display = ('user', 'service', 'time', 'delta', 'action')
+    list_display = ("user", "service", "time", "delta", "action")
     readonly_fields = list(map(lambda x: x.name, PointLog._meta.fields))
 
     def has_add_permission(self, request, obj=None):
@@ -172,7 +218,7 @@ class PointLogAdmin(admin.ModelAdmin):
 @admin.register(UserLog)
 class UserLogAdmin(admin.ModelAdmin):
     actions = None
-    list_display = ('user', 'level', 'time', 'ip', 'text')
+    list_display = ("user", "level", "time", "ip", "text")
     readonly_fields = list(map(lambda x: x.name, UserLog._meta.fields))
 
     def has_add_permission(self, request, obj=None):
@@ -184,13 +230,13 @@ class UserLogAdmin(admin.ModelAdmin):
 
 @admin.register(EmailDomain)
 class EmailDomainAdmin(admin.ModelAdmin):
-    list_display = ('domain', 'is_banned')
-    search_fields = ('domain', )
-    ordering = ('domain', )
+    list_display = ("domain", "is_banned")
+    search_fields = ("domain",)
+    ordering = ("domain",)
 
 
 @admin.register(InquiryMail)
 class InquiryMailAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created_at', 'user', 'topic', 'title')
-    fields = ('created_at', 'user', 'name', 'email', 'topic', 'title', 'content')
+    list_display = ("id", "created_at", "user", "topic", "title")
+    fields = ("created_at", "user", "name", "email", "topic", "title", "content")
     readonly_fields = fields
